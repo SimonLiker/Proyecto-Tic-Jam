38ed5a6c14a00d657b7283964097b5991a302c02
Simón Liker <126901295+SimonLiker@users.noreply.github.com>
2023-04-06 22:28:16 -0300

    started making enemy's script

---
+++ Assets/enemy1Script.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy1Script : MonoBehaviour
+{
+    public GameObject enemyBullet;
+    Ray playerDetection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //playerDetection = (gameObject.transform.position.x);
+    }
+}
