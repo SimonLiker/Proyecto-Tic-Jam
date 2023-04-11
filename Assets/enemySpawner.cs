dee8d7b6d46bd307b08b8f4d7a77c342b8423038
Simón Liker <126901295+SimonLiker@users.noreply.github.com>
2023-04-11 14:02:29 -0300

    spawner añadido

---
+++ Assets/enemySpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    float time = 0;
+    int timeInt = 0;
+
+    public GameObject basicEnemy;
+
+    int randSpawnPoint;
+
+    int counter = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        timeInt = Mathf.RoundToInt(time);
+
+        if (timeInt > 0 && timeInt % 4 == 0)
+        {
+            while (counter > 0)
+            {
+                randSpawnPoint = Random.Range(0, spawnPoints.Length);
+
+                Instantiate(basicEnemy, spawnPoints[randSpawnPoint].transform.position, Quaternion.identity);
+            }
+        }
+        else
+        {
+            counter = 1;
+        }
+    }
+}
